046c6c669f5f4bd697835897bc5f56c313cc7ee0
Nibras <nibrasahmad125@gmail.com>
2022-12-29 11:25:43 +0500

    test: testing the fullname to short (#2100)

--- shorten-name/name.go
+++ shorten-name/name.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Array!")
+	fullNameArray := []string{"Nibras Ahmed", "Ahmed Saleem", "Ahsan Yousaf", "Muhammad Husnain"}
+	shortNameArray := []string{}
+	for _, name := range fullNameArray {
+		parts := strings.Split(name, " ")
+		first, last := parts[0], parts[len(parts)-1]
+		shortNames := string(first[0]) + string(last[0])
+		shortNameArray = append(shortNameArray, shortNames)
+	}
+	fmt.Println(shortNameArray)
 }
